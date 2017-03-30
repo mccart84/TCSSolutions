ef0b6f69cefdef15d72c6e0beaf7cf82630734c2
Ryan <Ryan@DESKTOP>
2017-03-30 18:44:02 -0400

    Initial Commit

---
+++ TCSSolutions/Models/NavigationItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCSSolutions.Models
+{
+    public class NavigationItem
+    {
+        public int RecId { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string DisplayText { get; set; }
+        public bool IsActive { get; set; }
+        public int SortOrder { get; set; }
+        public int ParentId { get; set; }
+        public string Description { get; set; }
+        public bool IsSystem { get; set; }
+
+        public virtual ICollection<Section> Sections { get; set; }
+        public virtual ICollection<PageType> PageTypes { get; set; }
+    }
+}
